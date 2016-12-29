9cdb40338e9e9c2decac119e8672d2a0a1cfc1b4
zhouyang <zhoulygku@163.com>
2016-12-29 10:33:47 +0800

    用户管理界面开发完成

--- EasyUI/js/common/common.js
+++ EasyUI/js/common/common.js
@@ -4,10 +4,30 @@
  * 
  */
 window.onload = function(){
-	//首次加载
+	//首次加载 
 	$('#loading-mask').fadeOut();
 }
- 
+/*日期格式化处理*/
+ Date.prototype.format = function(format){ 
+    var o =  { 
+    "M+" : this.getMonth()+1, //month 
+    "d+" : this.getDate(), //day 
+    "h+" : this.getHours(), //hour 
+    "m+" : this.getMinutes(), //minute 
+    "s+" : this.getSeconds(), //second 
+    "q+" : Math.floor((this.getMonth()+3)/3), //quarter 
+    "S" : this.getMilliseconds() //millisecond 
+    };
+    if(/(y+)/.test(format)){ 
+    	format = format.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+    }
+    for(var k in o)  { 
+	    if(new RegExp("("+ k +")").test(format)){ 
+	    	format = format.replace(RegExp.$1, RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length)); 
+	    } 
+    } 
+    return format; 
+};
 
 var COMMON = {
  
@@ -23,7 +43,12 @@ var COMMON = {
 				closable: false
 			});
 
-	},	 
+	},
+// 格式化时间
+	formatDateTime : function(val,row){
+		var now = new Date(val);
+    	return now.format("yyyy-MM-dd hh:mm:ss");
+	},
 	/*菜单点击事件*/
 	treeClick:function(node){
 	
